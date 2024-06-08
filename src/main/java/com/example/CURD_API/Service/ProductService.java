77166f45f36849bc6810a3fc8d2f6ebf6bd8aff9
Nishant Sonar <nishantsonar19@gmail.com>
2024-06-08 11:29:12 +0530

    docker Implemented

--- src/main/java/com/example/CURD_API/Service/ProductService.java
+++ src/main/java/com/example/CURD_API/Service/ProductService.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @Service
 public class ProductService {
-    @Autowired(required=true)
+    @Autowired
     private ProductRepository productRepository;
 
     public List<Product> getAllProducts() {
